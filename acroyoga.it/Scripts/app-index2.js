5eae31eefe48207dbb3432b7f54857a35428bc83
Marco Campulla <mcampulla@gmail.com>
2017-01-16 18:32:16 +0100

    anguòarjs

---
+++ acroyoga.it/Scripts/app-index2.js
@@ -0,0 +1,29 @@
+﻿"use strict";
+
+var app = angular.module("Index2", []);
+
+app.controller('Index2Controller', function ($scope, $http) {
+
+    $scope.currentEvent = {};
+    $scope.ShowEvents = false;
+
+    $http({
+        url: "http://acroyoga.azurewebsites.net/odata/Events",
+        method: "GET"
+        //,headers: {
+        //    "X-ZUMO-AUTH": token
+        //}
+    }).then(function (result) {
+        $scope.events = angular.copy(result.data.value);
+        $scope.currentEvent = $scope.events[0];
+        $scope.ShowEvents = true;
+    }, function () { // failure
+        $scope.ShowEvents = false;
+        //deferred.reject("Errore durante la richiesta di elaborazione stagioni.");
+    });
+
+    $scope.changeEvent = function (index) {
+        $scope.currentEvent = $scope.events[index];
+    };
+
+});
