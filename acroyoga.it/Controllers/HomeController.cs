2236b373499ffea35b961e527815f7172f18c561
Marco Campulla <mcampulla@gmail.com>
2017-08-17 00:27:54 +0200

    email

--- acroyoga.it/Controllers/HomeController.cs
+++ acroyoga.it/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,10 +11,10 @@ namespace acroyoga.it.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
-        {
-            return View();
-        }
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
 
         public ActionResult Index2()
         {
@@ -20,8 +22,40 @@ namespace acroyoga.it.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(bool IsPost=true)
+        public ActionResult Index2(bool IsPost = true)
         {
+            string name = Request["form-name"];
+            string email = Request["form-email"];
+            string subject = Request["form-subject"];
+            string msg = Request["form-message"];
+
+            string body = string.Format(@"<table><tr><td>Nome</td><td>{0}</td></tr>
+                                                <tr><td>Email</td><td>{1}</td></tr>
+                                                <tr><td>Oggetto</td><td>{2}</td></tr>
+                                                <tr><td>Messaggio</td><td>{3}</td></tr></table>"
+                                        , name, email, subject, msg);
+
+            SmtpClient client = new SmtpClient("smtp.acroyogapadova.it");
+            //client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.EnableSsl = false;
+            client.Timeout = 10000;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential("188202@aruba.it", "89485c966c");
+
+            try
+            {
+                MailMessage mm = new MailMessage("info@acroyogapadova.it", "info@acroyogapadova.it", string.Format("Messaggio da {0}", email), body);
+                //mm.BodyEncoding = UTF8Encoding.UTF8;
+                mm.IsBodyHtml = true;
+                //mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                string s = ex.Message;
+                ViewBag.Error = ex.Message;
+            }
             return View();
         }
 
